48fd79fd5dbcbca470f16b44d5a57beccac40a09
darcn <darcn@users.noreply.github.com>
2015-11-18 17:08:59 -0500

    Changed servo code

--- thenderTelop2015.java
+++ thenderTelop2015.java
@@ -10,13 +10,13 @@ import com.qualcomm.robotcore.hardware.Servo;
  * Created by admin on 10/14/2015.
  */
 
-public class thenderTelop2015 extends OpMode {
+public class thunderTelop2015 extends OpMode {
 
-    DcMotor L;//left wheel motor
-    DcMotor R;//right wheel motor
-    DcMotor A;//vertical movement of the scoring mechanism
-    DcMotor B;//horizontal movement of the scoring mechanism
-    Servo servo;//slightly tilts scoring mechanism scoop
+    private DcMotor L;//left wheel motor
+    private DcMotor R;//right wheel motor
+    private DcMotor A;//vertical movement of the scoring mechanism
+    private DcMotor B;//horizontal movement of the scoring mechanism
+    private Servo servo;//slightly tilts scoring mechanism scoop
 
 
     @Override
@@ -26,7 +26,7 @@ public class thenderTelop2015 extends OpMode {
         A = hardwareMap.dcMotor.get("A");
         B = hardwareMap.dcMotor.get("B");
         servo = hardwareMap.servo.get("servo");
-        
+
         //reverses the right motor
         R.setDirection(DcMotor.Direction.REVERSE);
     }
@@ -34,23 +34,29 @@ public class thenderTelop2015 extends OpMode {
     public void loop(){
         /*logarithmic control for drivetrain; the gamepad  
         value is cubed so negative numbers stay negative*/
-        double left = Math.pow(gamepad1.left_stick_y, 3); 
+        double left = Math.pow(gamepad1.left_stick_y, 3);
         double right = Math.pow(gamepad1.right_stick_y, 3);
-        
+
         //gamepad values modified into motor/servo values
         double motora = gamepad2.left_stick_y*0.5;
         double motorb = -gamepad2.left_stick_x*0.25;
-        double servoposition = -gamepad2.right_stick_y+0.5;
+        double servoposition = 0;
 
         //range of motor values is between -1 and 1
         left = Range.clip(left, -1, 1);
         right= Range.clip(right, -1, 1);
         motora=Range.clip(motora, -1, 1);
         motorb=Range.clip(motorb, -1, 1);
-        
+
         //range of servo values is between 0 and 1
         servoposition=Range.clip(servoposition, 0, 1);
 
+        if (-gamepad2.right_stick_y>0){
+            servoposition = servoposition+0.01;
+        }
+        if (-gamepad2.right_stick_y<0){
+            servoposition = servoposition-0.01;
+        }
         servo.setPosition(servoposition);
         A.setPower(motora);
         B.setPower(motorb);
